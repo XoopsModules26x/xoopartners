36c87e5c3416631f9a07c40a65332d56c0145346
DuGris <dugris@users.noreply.github.com>
2012-11-26 11:20:53 +0000

    Fix : Meta pour la page d'accueil du module

--- xoopartners/index.php
+++ xoopartners/index.php
@@ -20,16 +20,35 @@
 include dirname(__FILE__) . DIRECTORY_SEPARATOR . 'header.php';
 
 if ($Partners_config['xoopartners_category']['use_categories']) {    $category_id = $system->CleanVars($_REQUEST, 'category_id', 0, 'int');
-    $xoops->tpl->assign('categories', $categories_handler->GetCategories() );
+    $categories = $categories_handler->GetCategories();
+    $partners = $partners_handler->GetPartners( $category_id );
     $xoops->tpl->assign('category_id', $category_id);
-    $xoops->tpl->assign('partners', $partners_handler->GetPartners( $category_id ) );
+    $xoops->tpl->assign('categories', $categories);
+    $xoops->tpl->assign('partners', $partners);
 
     if ( $Partners_config['xoopartners_category']['display_mode'] == "select" ) {        $xoops->tpl->assign('category_header', '<div class="txtcenter"><select name="category_id" onchange=\'window.location.href="index.php?category_id="+this.options[this.selectedIndex].value\'>' );
         $xoops->tpl->assign('category_footer', '</select></div>' );
     } elseif ( $Partners_config['xoopartners_category']['display_mode'] == "table" ) {        $xoops->tpl->assign('category_header', '<table class="outer">' );
         $xoops->tpl->assign('category_footer', '</table>' );
     }
-} else {    $xoops->tpl->assign('partners', $partners_handler->GetPartners() );
+} else {    $partners = $partners_handler->GetPartners();
+    $xoops->tpl->assign('partners', $partners);
 }
+
+$description = '';
+if ( isset($categories) ) {    foreach ($categories as $k => $category ) {
+        $description .= $category['xoopartners_category_title'];
+    }
+}
+$i=0;
+foreach ($partners as $k => $partner ) {
+    $description .= $partner['xoopartners_title'];
+    $i++;
+    if ( $i < count($partners) ) {
+        $description .= ', ';
+    }
+}
+$xoops->theme->addMeta($type = 'meta', 'description', getMetaDescription( $description ) );
+$xoops->theme->addMeta($type = 'meta', 'keywords', getMetaKeywords( $description ) );
 include dirname(__FILE__) . DIRECTORY_SEPARATOR . 'footer.php';
 ?>
