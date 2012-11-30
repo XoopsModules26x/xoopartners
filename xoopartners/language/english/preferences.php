8f1b3ab28479c72d7927450718050506a2409478
DuGris <dugris@users.noreply.github.com>
2012-11-30 01:32:19 +0000

    Fix : Configuration language

--- xoopartners/language/english/preferences.php
+++ xoopartners/language/english/preferences.php
@@ -17,43 +17,46 @@
  * @version         $Id$
  */
 
-define("_MI_XOO_CONFIG_MAINPAGE",             "Main page");
-define("_MI_XOO_CONFIG_CATEGORY",             "Categories");
-define("_MI_XOO_CONFIG_PARTNER",              "Partners");
+if (!defined("_XOO_CONFIG_SAVED")) {
+    define("_XOO_CONFIG_SAVED",            "Configuration was saved");
+}
+define("_XOO_CONFIG_MAINPAGE",             "Main page");
+define("_XOO_CONFIG_CATEGORY",             "Categories");
+define("_XOO_CONFIG_PARTNER",              "Partners");
 
-define("_MI_XOO_CONFIG_MAIN",                 "Display pages in main menu");
-define("_MI_XOO_CONFIG_WELCOME",              "Text to display in index page");
+define("_XOO_CONFIG_MAIN",                 "Display pages in main menu");
+define("_XOO_CONFIG_WELCOME",              "Text to display in index page");
 
-define("_MI_XOO_CONFIG_CATEGORY_MODE",        "Display mode of the main page");
-define("_MI_XOO_CONFIG_MODE_NEWS",            "News");
-define("_MI_XOO_CONFIG_MODE_LIST",            "List");
-define("_MI_XOO_CONFIG_MODE_TABLE",           "Table");
-define("_MI_XOO_CONFIG_MODE_SELECT",          "Select");
-define("_MI_XOO_CONFIG_MODE_IMAGES",          "Images");
+define("_XOO_CONFIG_CATEGORY_MODE",        "Display mode of the main page");
+define("_XOO_CONFIG_MODE_NEWS",            "News");
+define("_XOO_CONFIG_MODE_LIST",            "List");
+define("_XOO_CONFIG_MODE_TABLE",           "Table");
+define("_XOO_CONFIG_MODE_SELECT",          "Select");
+define("_XOO_CONFIG_MODE_IMAGES",          "Images");
 
-define("_MI_XOO_CONFIG_PARTNER_MODE",         "Display mode of the main page");
+define("_XOO_CONFIG_PARTNER_MODE",         "Display mode of the main page");
 
-define("_MI_XOO_CONFIG_PAGE_LIMIT",           "Limit");
-define("_MI_XOO_CONFIG_PAGE_LIMIT_DESC",      "Number of items to display on the main page (0 = no limit)");
-define("_MI_XOO_CONFIG_USE_CATEGORIES",       "Use categories");
-define("_MI_XOO_CONFIG_MAIN_MENU",            "Display categories in main menu");
+define("_XOO_CONFIG_PAGE_LIMIT",           "Limit");
+define("_XOO_CONFIG_PAGE_LIMIT_DESC",      "Number of items to display on the main page (0 = no limit)");
+define("_XOO_CONFIG_USE_CATEGORIES",       "Use categories");
+define("_XOO_CONFIG_MAIN_MENU",            "Display categories in main menu");
 
 // RLD
-define("_MI_XOO_CONFIG_RLD",                 "Rate / Like Dislike");
-define("_MI_XOO_CONFIG_RLD_MODE",            "Rate / Like Dislike");
-define("_MI_XOO_CONFIG_RLD_NONE",            "None");
-define("_MI_XOO_CONFIG_RLD_RATE",            "Rates");
-define("_MI_XOO_CONFIG_RLD_LIKEDISLIKE",     "Like / Dislike");
-define("_MI_XOO_CONFIG_RATE_SCALE",          "Number of stars for vote");
+define("_XOO_CONFIG_RLD",                 "Rate / Like Dislike");
+define("_XOO_CONFIG_RLD_MODE",            "Rate / Like Dislike");
+define("_XOO_CONFIG_RLD_NONE",            "None");
+define("_XOO_CONFIG_RLD_RATE",            "Rates");
+define("_XOO_CONFIG_RLD_LIKEDISLIKE",     "Like / Dislike");
+define("_XOO_CONFIG_RATE_SCALE",          "Number of stars for vote");
 
 // QRCode
-define("_MI_XOO_CONFIG_QRCODE",              "QR Code");
-define("_MI_XOO_CONFIG_QRCODE_USE",          "Use QR Code");
-define("_MI_XOO_CONFIG_QRCODE_ECL",          "Error Correction Level");
-define("_MI_XOO_CONFIG_QRCODE_ECL_L",        "Smallest");
-define("_MI_XOO_CONFIG_QRCODE_ECL_M",        "Medium");
-define("_MI_XOO_CONFIG_QRCODE_ECL_Q",        "Hight");
-define("_MI_XOO_CONFIG_QRCODE_ECL_H",        "Best");
-define("_MI_XOO_CONFIG_QRCODE_MATRIX",       "Image width");
-define("_MI_XOO_CONFIG_QRCODE_MARGIN",       "Image margin (px)");
+define("_XOO_CONFIG_QRCODE",              "QR Code");
+define("_XOO_CONFIG_QRCODE_USE",          "Use QR Code");
+define("_XOO_CONFIG_QRCODE_ECL",          "Error Correction Level");
+define("_XOO_CONFIG_QRCODE_ECL_L",        "Smallest");
+define("_XOO_CONFIG_QRCODE_ECL_M",        "Medium");
+define("_XOO_CONFIG_QRCODE_ECL_Q",        "Hight");
+define("_XOO_CONFIG_QRCODE_ECL_H",        "Best");
+define("_XOO_CONFIG_QRCODE_MATRIX",       "Image width");
+define("_XOO_CONFIG_QRCODE_MARGIN",       "Image margin (px)");
 ?>
